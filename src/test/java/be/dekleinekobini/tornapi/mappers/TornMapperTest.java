16db6e9a471bcdcdbbf0cbb95100bacca41ff9e2
DeKleineKobini <kobedepeuter@outlook.com>
2021-12-23 18:51:03 +0100

    Switch to a new system for torn

---
+++ src/test/java/be/dekleinekobini/tornapi/mappers/TornMapperTest.java
@@ -0,0 +1,129 @@
+package be.dekleinekobini.tornapi.mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class TornMapperTest {
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofBank() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofCards() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofChainReport() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofCompanies() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofCompetition() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofEducation() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofFactionTree() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofGyms() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofHonors() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofItems() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofLogCategories() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofLogTypes() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofMedals() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofOrganisedCrimes() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofPawnShop() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofPokerTables() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofProperties() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofRackets() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofRaids() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofRankedWarReport() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofStats() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofStocks() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofTerritory() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofTerritoryWars() throws JsonProcessingException {
+    }
+
+}
