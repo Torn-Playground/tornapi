b4e79ab6b74113ed60e31892d1abad95df678ea8
DeKleineKobini <kobedepeuter@outlook.com>
2021-12-23 18:30:37 +0100

    Switch to a new system for companies

---
+++ src/test/java/be/dekleinekobini/tornapi/mappers/CompanyMapperTest.java
@@ -0,0 +1,49 @@
+package be.dekleinekobini.tornapi.mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class CompanyMapperTest {
+
+    @Test
+    @Disabled("Not a director, unable to implement.")
+    void ofApplications() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofCompanies() throws JsonProcessingException {
+    }
+
+    @Test
+    @Disabled("Not a director, unable to implement.")
+    void ofDetailed() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofEmpoyees() throws JsonProcessingException {
+    }
+
+    @Test
+    @Disabled("Not a director, unable to implement.")
+    void ofNewsFull() throws JsonProcessingException {
+    }
+
+    @Test
+    @Disabled("Not a director, unable to implement.")
+    void ofNews() throws JsonProcessingException {
+    }
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofProfile() throws JsonProcessingException {
+    }
+
+    @Test
+    @Disabled("Not a director, unable to implement.")
+    void ofStocks() throws JsonProcessingException {
+    }
+
+}
