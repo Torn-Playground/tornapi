c8df3f4ae2f2561d5333c8f90ca5ca1fcf657efd
DeKleineKobini <kobedepeuter@outlook.com>
2021-12-23 18:31:15 +0100

    Switch to a new system for companies

---
+++ src/test/java/be/dekleinekobini/tornapi/mappers/PropertiesMapperTest.java
@@ -0,0 +1,14 @@
+package be.dekleinekobini.tornapi.mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class PropertiesMapperTest {
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofProperty() throws JsonProcessingException {
+    }
+
+}
