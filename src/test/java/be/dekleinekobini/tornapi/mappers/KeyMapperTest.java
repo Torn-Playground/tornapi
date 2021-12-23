682abdee769bd5b46b855f5cdace105648a331f5
DeKleineKobini <kobedepeuter@outlook.com>
2021-12-23 18:46:38 +0100

    Add key section test

---
+++ src/test/java/be/dekleinekobini/tornapi/mappers/KeyMapperTest.java
@@ -0,0 +1,14 @@
+package be.dekleinekobini.tornapi.mappers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class KeyMapperTest {
+
+    @Disabled("Not yet implemented")
+    @Test
+    void ofInfo() throws JsonProcessingException {
+    }
+
+}
