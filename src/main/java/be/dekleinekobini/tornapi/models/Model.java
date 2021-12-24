33ade96e355d5891220083af0eb4c2f41dd751d5
DeKleineKobini <kobedepeuter@outlook.com>
2021-12-24 19:32:24 +0100

    Add key model

--- src/main/java/be/dekleinekobini/tornapi/models/Model.java
+++ src/main/java/be/dekleinekobini/tornapi/models/Model.java
@@ -1,18 +1,7 @@
 package be.dekleinekobini.tornapi.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class Model {
-
-    protected final static ObjectMapper OBJECT_MAPPER = JsonMapper.builder()
-            .findAndAddModules()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
-            .build();
-
 }
