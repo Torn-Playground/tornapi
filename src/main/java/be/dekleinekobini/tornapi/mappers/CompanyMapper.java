a46b760f01bd653ea1114d69ae155cd66825e632
DeKleineKobini <kobedepeuter@outlook.com>
2022-02-27 20:39:09 +0100

    Update companies mapper

--- src/main/java/be/dekleinekobini/tornapi/mappers/CompanyMapper.java
+++ src/main/java/be/dekleinekobini/tornapi/mappers/CompanyMapper.java
@@ -4,6 +4,7 @@ import be.dekleinekobini.tornapi.models.companies.Company;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.Collection;
 import java.util.Map;
 
 public class CompanyMapper extends ModelMapper {
@@ -11,9 +12,9 @@ public class CompanyMapper extends ModelMapper {
     private CompanyMapper() {
     }
 
-    public static Map<Long, Company> ofCompany(JsonNode json) {
-        return OBJECT_MAPPER.convertValue(json.get("company"), new TypeReference<>() {
-        });
+    public static Collection<Company> ofCompany(JsonNode json) {
+        return OBJECT_MAPPER.convertValue(json.get("company"), new TypeReference<Map<Long, Company>>() {
+        }).values();
     }
 
 }
