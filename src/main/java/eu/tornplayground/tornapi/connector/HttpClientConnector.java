c64f34278fc7f15e7fd113edaab033935610c780
DeKleineKobini <kobedepeuter@outlook.com>
2022-10-19 09:53:42 +0200

    Support http 1 client connector.

--- src/main/java/eu/tornplayground/tornapi/connector/HttpClientConnector.java
+++ src/main/java/eu/tornplayground/tornapi/connector/HttpClientConnector.java
@@ -9,9 +9,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import static java.net.http.HttpClient.Version.HTTP_1_1;
+import static java.net.http.HttpClient.Version.HTTP_2;
+
 public class HttpClientConnector implements ApiConnector {
 
-    private final HttpClient client = HttpClient.newBuilder().build();
+    private final HttpClient client;
+
+    public HttpClientConnector(boolean useHttp2) {
+        client = HttpClient.newBuilder()
+                .version(useHttp2 ? HTTP_2 : HTTP_1_1)
+                .build();
+    }
+
+    public HttpClientConnector() {
+        this(true);
+    }
 
     @Override
     public JsonNode connect(String url) throws IOException, InterruptedException {
